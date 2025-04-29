98095841c8893768b182b3bb496dc09d04b34aa5
Tarun Kumar <tarunnimbal123@gmail.com>
2025-04-29 12:50:28 +0530

    Adding signin and signup functionality-working and adding other routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,15 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isPublicRoute = createRouteMatcher(["/", "/signin", "/signup", "/home"]);
+const isPublicRoute = createRouteMatcher([
+  "/",
+  "/sign-in",
+  "/sign-up",
+  "/home",
+]);
 const isPublicApiRoute = createRouteMatcher(["/api/videos"]);
-export default clerkMiddleware((auth, req) => {
-  const { userId }: any = auth();
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
   const currentUrl = new URL(req.url);
   const isAccessingDashnoard = currentUrl.pathname === "/home";
   const isApiRequest = currentUrl.pathname.startsWith("/api");
@@ -16,11 +21,11 @@ export default clerkMiddleware((auth, req) => {
   // User Not logged in
   if (!userId) {
     if (!isPublicRoute(req) && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/signin", req.url));
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
 
     if (isApiRequest && !isPublicApiRoute(req)) {
-      return NextResponse.redirect(new URL("/signin", req.url));
+      return NextResponse.redirect(new URL("/sign-in", req.url));
     }
   }
 });
